6b70b096cabd2741efcbdf90145d03adf16a2da3
SABELO MAWELA <MAWELAS63@GMAIL.COM>
2024-05-23 10:05:07 +0200

    "Updated relative paths, and added Redux-like state management code to index.js"

--- index.js
+++ index.js
@@ -24,3 +24,39 @@ const subtractButton = document.querySelector('.subtractbtn');
 subtractButton.addEventListener('click', () => {
     console.log("hey")
 });
+
+
+  const counterReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case INCREMENT:
+        return { count: state.count + 1 };
+      case DECREMENT:
+        return { count: state.count - 1 };
+      default:
+        return state;
+    }
+  };
+
+  function createStore(reducer) {
+    let state;
+    let listeners = [];
+  
+    const getState = () => state;
+  
+    const dispatch = (action) => {
+      state = reducer(state, action);
+      listeners.forEach(listener => listener());
+    };
+  
+    const subscribe = (listener) => {
+      listeners.push(listener);
+      return () => {
+        listeners = listeners.filter(l => l !== listener);
+      };
+    };
+  
+    // Initialize the state
+    dispatch({});
+  
+    return { getState, dispatch, subscribe };
+  }
