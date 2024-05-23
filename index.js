147066fb561f79c47033924568890d1e279410b1
SABELO MAWELA <MAWELAS63@GMAIL.COM>
2024-05-23 02:04:32 +0200

    "Added script tag to index.html" added index.js an add event listeners to see if my bottons work

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const button = document.querySelector('.plusbtn');
+console.log(button)
+
+
+button.addEventListener('click', () => {
+    console.log ("hello")
+
+});
+
+
+
+const subtractButton = document.querySelector('.subtractbtn');
+
+
+subtractButton.addEventListener('click', () => {
+    console.log ("hey")
+});
